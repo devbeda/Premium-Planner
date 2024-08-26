034f60d8d915fb94d1701bdb0f59feec5e8e2705
Beda Prakash Sahu <164047663+devbeda@users.noreply.github.com>
2024-08-26 14:08:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ dotenv.config({
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://frontend-premium-planner.vercel.app",
     credentials: true,
     // Add other headers if needed.  //
   })
